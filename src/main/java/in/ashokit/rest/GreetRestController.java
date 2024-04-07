d89a9aa53c60b715969de2f8808a9e3c9da8dd87
swamygitaccount <113229573+swamygitaccount@users.noreply.github.com>
2024-04-07 16:46:52 +0300

    Update GreetRestController.java

--- src/main/java/in/ashokit/rest/GreetRestController.java
+++ src/main/java/in/ashokit/rest/GreetRestController.java
@@ -1,15 +1,14 @@
 package in.ashokit.rest;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 @RestController
 @RequestMapping
 public class GreetRestController {
 	
+String s = "hi";
 	@Autowired 
 	private Environment env;
 	
