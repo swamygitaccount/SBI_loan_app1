39047bcf953081bb66468538571bddbe15a7ada4
swamy <ashokitschool@gmail.com>
2024-04-07 16:20:14 +0300

    initial commit

---
+++ src/main/java/in/ashokit/rest/GreetRestController.java
@@ -0,0 +1,28 @@
+package in.ashokit.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class GreetRestController {
+	
+	@Autowired 
+	private Environment env;
+	
+	
+	@GetMapping("/greet/")  
+	public String getGreetMsg() {
+		
+		
+	// to get the port number of the server
+		String port = env.getProperty("server.port");
+		
+		// returning with port number (in which server greet api got executed, port num will give me)
+		return "Good Evening ("+ port +")";
+	}
+
+}
